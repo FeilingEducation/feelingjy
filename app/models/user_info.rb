978c94028a9644c8e0920855f8f44984cb97ad3b
Leon Yang <leon.gh.yang@gmail.com>
2017-08-30 21:56:51 -0500

    Use enum for gender

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -1,16 +1,17 @@
 class UserInfo < ApplicationRecord
 
+  enum gender: { male: 0, female: 1, other: 2 }
+
   mount_uploader :avatar, AvatarUploader
 
   belongs_to :user, foreign_key: 'id'
 
   validates_presence_of :id
+  validates_presence_of :gender
 
   validates :first_name, :presence => true,
                          :length => { :maximum => 20 }
   validates :last_name, :presence => true,
                         :length => { :maximum => 20 }
-  validates :gender, :presence => true,
-                     :inclusion => { in: (0..2) }
 
 end
