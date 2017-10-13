9d23616aa8dbeb33838ad2673c102c029115520b
Leon Yang <leon.gh.yang@gmail.com>
2017-10-13 01:07:33 -0500

    Use postgresql

--- Gemfile
+++ Gemfile
@@ -8,8 +8,6 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.3'
-# Use mysql as the database for Active Record
-gem 'mysql2', '>= 0.3.18', '< 0.5'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
@@ -58,3 +56,4 @@ gem 'carrierwave'
 gem 'mini_magick'
 gem 'bootstrap'
 gem 'jquery-rails'
+gem 'pg'
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20171005060543) do
 
-  create_table "attachments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "attachments", force: :cascade do |t|
     t.string "file"
     t.string "md5"
     t.string "file_type"
@@ -23,24 +26,24 @@ ActiveRecord::Schema.define(version: 20171005060543) do
     t.index ["attachable_type", "attachable_id"], name: "index_attachments_on_attachable_type_and_attachable_id"
   end
 
-  create_table "chat_lines", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "chat_lines", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "content"
     t.bigint "chat_id"
     t.bigint "user_info_id"
     t.index ["chat_id"], name: "index_chat_lines_on_chat_id"
     t.index ["user_info_id"], name: "index_chat_lines_on_user_info_id"
   end
 
-  create_table "chats", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "chats", force: :cascade do |t|
     t.integer "consult_transaction_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["consult_transaction_id"], name: "index_chats_on_consult_transaction_id"
   end
 
-  create_table "consult_transactions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "consult_transactions", force: :cascade do |t|
     t.integer "instructor_id", null: false
     t.integer "student_id", null: false
     t.text "privacy_policy"
@@ -56,7 +59,7 @@ ActiveRecord::Schema.define(version: 20171005060543) do
     t.index ["student_id"], name: "index_consult_transactions_on_student_id"
   end
 
-  create_table "instructor_infos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "instructor_infos", force: :cascade do |t|
     t.text "consult_experience"
     t.text "previous_applications"
     t.text "previous_offers"
@@ -67,10 +70,10 @@ ActiveRecord::Schema.define(version: 20171005060543) do
     t.string "page_title"
     t.text "available_time_slots"
     t.text "busy_events"
-    t.boolean "is_early_consult", default: false, null: false
-    t.boolean "is_brainstorm_consult", default: false, null: false
-    t.boolean "is_essay_consult", default: false, null: false
-    t.boolean "is_visa_consult", default: false, null: false
+    t.boolean "is_early_consult"
+    t.boolean "is_brainstorm_consult"
+    t.boolean "is_essay_consult"
+    t.boolean "is_visa_consult"
     t.integer "consult_capacity_min"
     t.integer "consult_capacity_max"
     t.integer "consult_term_min"
@@ -87,13 +90,13 @@ ActiveRecord::Schema.define(version: 20171005060543) do
     t.integer "price_base"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "avg_rating", limit: 24
+    t.float "avg_rating"
     t.string "url_linked_in"
     t.string "page_background"
     t.text "about_me"
   end
 
-  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "messages", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "receiver_id"
     t.text "content"
@@ -101,7 +104,7 @@ ActiveRecord::Schema.define(version: 20171005060543) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "payments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "payments", force: :cascade do |t|
     t.string "payable_type"
     t.bigint "payable_id"
     t.integer "method"
@@ -112,7 +115,7 @@ ActiveRecord::Schema.define(version: 20171005060543) do
     t.index ["payable_type", "payable_id"], name: "index_payments_on_payable_type_and_payable_id"
   end
 
-  create_table "private_policies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "private_policies", force: :cascade do |t|
     t.integer "instructor_id", null: false
     t.text "content"
     t.boolean "pending", default: true, null: false
@@ -121,7 +124,7 @@ ActiveRecord::Schema.define(version: 20171005060543) do
     t.index ["instructor_id"], name: "index_private_policies_on_instructor_id"
   end
 
-  create_table "user_infos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "user_infos", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "current_city"
@@ -144,7 +147,7 @@ ActiveRecord::Schema.define(version: 20171005060543) do
     t.boolean "need_visa_consult"
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
