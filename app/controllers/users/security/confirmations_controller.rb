cbe34db5c4d92b877f200442c6bbe78381d699e9
Nadeem Yasin <nadeem@westagilelabs.com>
2017-10-16 00:44:32 +0500

    Removing confirmation notice.

--- app/controllers/users/security/confirmations_controller.rb
+++ app/controllers/users/security/confirmations_controller.rb
@@ -1,6 +1,6 @@
 class Users::Security::ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
-    root_path(notice: "Please confirm your email.")
+    root_path
   end
 end
