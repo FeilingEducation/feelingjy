dc060b8523999c91b1850fd32ac3c09a31f20cff
Nadeem Yasin <nadeem@westagilelabs.com>
2017-10-22 19:25:42 +0500

    Completing step 24

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022115042) do
+ActiveRecord::Schema.define(version: 20171022140648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 20171022115042) do
     t.integer "max_price"
     t.integer "fix_price"
     t.string "work_frequency"
+    t.boolean "first_std_discount", default: true
   end
 
   create_table "messages", force: :cascade do |t|
