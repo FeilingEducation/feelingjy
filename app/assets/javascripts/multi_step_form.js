b864cd3b8eef046a920e054a3c7122d58e9f7084
Nadeem Yasin <nadeem@westagilelabs.com>
2017-10-22 21:16:13 +0500

    Completing server side work for instructor info

--- app/assets/javascripts/multi_step_form.js
+++ app/assets/javascripts/multi_step_form.js
@@ -3,13 +3,15 @@
 // navigate through multi-step-form
 $(document).on('click', '.multi-step-form .step-navigate', function () {
   var $this = $(this);
-  if ($this.is('.submit')) {
-    $this.closest('form').submit();
-    return;
-  }
+  // if ($this.is('.submit')) {
+  //   $this.closest('form').submit();
+  //   return;
+  // }
   var $curr_form = $this.closest('.multi-step-form');
   var $target_form = $($this.data('target'));
-  $curr_form.toggleClass('current-step');
-  $target_form.toggleClass('current-step');
+  if(!$this.is('.submit')) {
+    $curr_form.toggleClass('current-step');
+    $target_form.toggleClass('current-step');
+  }
   $target_form.find('input, select').filter(':first').focus();
 });
