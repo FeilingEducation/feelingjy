f764acc10c1aafbeaff390cf792d5da515b2b97e
Nadeem Yasin <nadeem@westagilelabs.com>
2017-10-29 17:16:22 +0500

    Adding logic, not to show the notification in the main header if user is already on messages screen

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -13,8 +13,9 @@ App.messages = App.cable.subscriptions.create("MessagesChannel", {
 
     // Clear the message text area
     $('#message-box-area').val('')
-
-    $('.nav-message').addClass('message-notification')
+    if(window.location.pathname != "/messages"){
+      $('.nav-message').addClass('message-notification')
+    }
 
   }
 });
