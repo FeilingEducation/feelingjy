107426aef061df6db8c39156c21835f1c41a0596
Nadeem Yasin <nadeem@westagilelabs.com>
2017-12-21 21:49:33 +0500

    Adding services

--- app/helpers/instructor_infos_helper.rb
+++ app/helpers/instructor_infos_helper.rb
@@ -2,18 +2,18 @@ module InstructorInfosHelper
 
   def services_of(instructor)
     services = []
-    if instructor.is_early_consult
+    # if instructor.is_early_consult
       services << '前期咨询'
-    end
-    if instructor.is_brainstorm_consult
+    # end
+    # if instructor.is_brainstorm_consult
       services << '头脑风暴'
-    end
-    if instructor.is_essay_consult
+    # end
+    # if instructor.is_essay_consult
       services << '文书改写'
-    end
-    if instructor.is_visa_consult
+    # end
+    # if instructor.is_visa_consult
       services << '签证咨询'
-    end
+    # end
     # if services.empty?
     #   services << '暂无服务'
     # end
