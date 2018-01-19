0153af290d8ab162c9ff1823df81b7bf64422d8f
Nadeem Yasin <nadeem@westagilelabs.com>
2018-01-20 02:45:00 +0500

    Adding some logs

--- config/deploy.rb
+++ config/deploy.rb
@@ -69,7 +69,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart -e production'
+      invoke 'puma:restart'
     end
   end
 
