b67c88decfc1abb2e5c1769f8194b782ea03481f
Nadeem Yasin <nadeem@westagilelabs.com>
2018-03-19 22:48:22 +0500

    ADded rack rewrite gem

--- config/initializers/rewrite.rb
+++ config/initializers/rewrite.rb
@@ -1,2 +1,6 @@
-# Redirect all http traffic to https
-r301 %r{.*}, 'https://www.feelingyt.com$&', :scheme => 'http'
+require 'rack/rewrite'
+
+use Rack::Rewrite do
+  # Redirect all http traffic to https
+  r301 %r{.*}, 'https://www.feelingyt.com$&', :scheme => 'http'
+end
