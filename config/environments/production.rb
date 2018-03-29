95111ff416c58624fc31828b0ed4f52fdd72a306
Nadeem Yasin <nadeem@westagilelabs.com>
2018-03-30 00:45:15 +0500

    Adding some configurations for sendcloud

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
+      :address        => 'smtp.sendcloud.net', #'smtp.sendgrid.net',
+      :port           => '25', #'587',
       :authentication => :plain,
       :user_name      => Rails.application.secrets.sendgrid_username,
       :password       => Rails.application.secrets.sendgrid_password,
