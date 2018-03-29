c49489c4816cfaf9c986272b1ea6da7357ba2fae
Nadeem Yasin <nadeem@westagilelabs.com>
2018-03-30 01:16:21 +0500

    Updating smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,9 @@ Rails.application.configure do
       :user_name      => Rails.application.secrets.sendgrid_username,
       :password       => Rails.application.secrets.sendgrid_password,
       :domain         => Rails.application.secrets.domain_name,
-      :enable_starttls_auto => false
+      :authentication       => "plain",
+      :enable_starttls_auto => true,
+      :openssl_verify_mode  => 'none'
   }
 
   config.eager_load = true
