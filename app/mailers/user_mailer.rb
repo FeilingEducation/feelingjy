a19ad0064f56903cb957e25c5a5f3a5922995528
Vincent Xia <vincentxia@Vincents-MacBook-Pro.local>
2018-04-16 03:58:46 -0700

    email template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < Devise::Mailer
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
-
-  def confirmation_instructions(record, token, opts={})
-   @token = token
-   #you can add your instance variables here
-   @first_name = UserInfo.find_by_id(record.id)
-   devise_mail(record, :confirmation_instructions, opts)
-  end
+  # 
+  # def confirmation_instructions(record, token, opts={})
+  #  @token = token
+  #  #you can add your instance variables here
+  #  @first_name = UserInfo.find_by_id(record.id)
+  #  devise_mail(record, :confirmation_instructions, opts)
+  # end
 end
