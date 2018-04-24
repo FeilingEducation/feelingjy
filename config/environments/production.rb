04629427073e67ebb0fd65e60f2a3470be58e1db
Nadeem Yasin <nadeem@westagilelabs.com>
2018-04-24 23:31:16 +0500

    some config changes for chat

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://www.feelingyt.com/cable'
-  config.action_cable.allowed_request_origins = [ 'wss://www.feelingyt.com/cable']
+  config.action_cable.allowed_request_origins = [ 'https://www.feelingyt.com/cable']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true # { port: 443}
