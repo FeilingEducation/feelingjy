41c3ffa957647412fe2227a2c0135d4c10a83413
Nadeem Yasin <nadeem@westagilelabs.com>
2018-04-26 00:20:06 +0500

    Some more change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,9 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://www.feelingyt.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://www.feelingyt.com/cable']
+  # config.action_cable.allowed_request_origins = [ 'https://www.feelingyt.com/cable']
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # https://github.com/websocket-rails/websocket-rails/issues/333
