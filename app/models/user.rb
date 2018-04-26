8a963aab86a62a5b1c6b6fd8b0f0cce147cba390
Vincent Xia <vincentxia@Vincents-MacBook-Pro.local>
2018-04-26 01:59:15 -0700

    review

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,39 +21,105 @@ class User < ApplicationRecord
       efficiency = 0
       authenticity = 0
       cost_effectiveness = 0
-      if r.service_communication_rating.nil?
-        service = 0
-      else
+      if !r.service_communication_rating.nil?
         service = r.service_communication_rating
       end
 
-      if r.attitude_rating.nil?
-        attitude = 0
-      else
+      if !r.attitude_rating.nil?
         attitude = r.attitude_rating
       end
 
-      if r.efficiency_rating.nil?
-        efficiency = 0
-      else
+      if !r.efficiency_rating.nil?
         efficiency = r.efficiency_rating
       end
 
-      if r.authenticity_rating.nil?
-        authenticity = 0
-      else
+      if !r.authenticity_rating.nil?
         authenticity = r.authenticity_rating
       end
 
-      if r.cost_effectiveness_rating.nil?
-        cost_effectiveness = 0
-      else
+      if !r.cost_effectiveness_rating.nil?
         cost_effectiveness = r.cost_effectiveness_rating
       end
       i = i + 1
   		rating = (service + attitude + efficiency + authenticity + cost_effectiveness).to_f/5
-      final_rating = (final_rating + rating)/i;
+      final_rating = final_rating + rating;
   	end
+    final_rating = final_rating/i;
   	return final_rating
+  end
+
+  def get_service_rating
+    final_service_rating = 0
+    i = 0
+    self.reviews.each do |r|
+      service = 0
+      if !r.service_communication_rating.nil?
+        service = r.service_communication_rating
+      end
+      i = i + 1
+      final_service_rating = final_service_rating + service
+    end
+    final_service_rating = 20*final_service_rating/i;
+    return final_service_rating.to_f
+  end
+
+  def get_attitude_rating
+    final_attitude_rating = 0
+    i = 0
+    self.reviews.each do |r|
+      attitude = 0
+      if !r.attitude_rating.nil?
+        attitude = r.attitude_rating
+      end
+      i = i + 1
+      final_attitude_rating = final_attitude_rating + attitude
+    end
+    final_attitude_rating = 20*final_attitude_rating/i;
+    return final_attitude_rating.to_f
+  end
+
+  def get_efficiency_rating
+    final_efficiency_rating = 0
+    i = 0
+    self.reviews.each do |r|
+      efficiency = 0
+      if !r.efficiency_rating.nil?
+        efficiency = r.efficiency_rating
+      end
+      i = i + 1
+      final_efficiency_rating = final_efficiency_rating + efficiency
+    end
+    final_efficiency_rating = 20*final_efficiency_rating/i;
+    return final_efficiency_rating.to_f
+  end
+
+  def get_authenticity_rating
+    final_authenticity_rating = 0
+    i = 0
+    self.reviews.each do |r|
+      authenticity = 0
+      if !r.authenticity_rating.nil?
+        authenticity = r.authenticity_rating
+      end
+      i = i + 1
+      final_authenticity_rating = final_authenticity_rating + authenticity
+    end
+    final_authenticity_rating = 20*final_authenticity_rating/i;
+    return final_authenticity_rating.to_f
+  end
+
+  def get_cost_effectiveness_rating
+    final_cost_effectiveness_rating = 0
+    i = 0
+    self.reviews.each do |r|
+      cost_effectiveness = 0
+      if !r.cost_effectiveness_rating.nil?
+        cost_effectiveness = r.cost_effectiveness_rating
+      end
+      i = i + 1
+      final_cost_effectiveness_rating = final_cost_effectiveness_rating + cost_effectiveness
+    end
+    final_cost_effectiveness_rating = 20*final_cost_effectiveness_rating/i;
+    return final_cost_effectiveness_rating.to_f
   end
 end
