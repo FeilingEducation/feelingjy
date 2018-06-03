d4cbf92450b0d5eb4bdcaf3ff1c601c4dfaa54b8
Nadeem Yasin <nadeem@westagilelabs.com>
2018-06-03 14:28:34 +0500

    adding logo back

--- app/models/university.rb
+++ app/models/university.rb
@@ -2,36 +2,36 @@ class University < ApplicationRecord
 
   has_one :cover_photo, class_name: 'Picture', foreign_key: 'pictureable_id'
   has_many :gallery_images, class_name: 'Picture', foreign_key: 'pictureable_id'
-  # 
-  # accepts_nested_attributes_for :cover_photo
-  # accepts_nested_attributes_for :gallery_images
-  #
-  # has_attached_file :logo,
-  #     :storage => :s3,
-  #     s3_region: 'us-west-1',
-  #     s3_host_name: 's3-us-west-1.amazonaws.com',
-  #     styles: { medium: "300x300>", thumb: "100x100>" },
-  #     :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
-  #
-  # validates_with AttachmentSizeValidator, attributes: :logo, less_than: 1.megabytes
-  #
-  # # validates_attachment_file_name :logo, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
-  #
-  # do_not_validate_attachment_file_type :logo
-  #
-  #
-  #
-  #
-  #
-  # def s3_credentials
-  #   {:bucket => Rails.application.secrets.s3_bucket_name,
-  #   :access_key_id => Rails.application.secrets.aws_access_key_id ,
-  #   :secret_access_key => Rails.application.secrets.aws_secret_access_key}
-  # end
-  #
-  # def name local='en'
-  #   local == 'en' ? name_en : name_cn
-  # end
+
+  accepts_nested_attributes_for :cover_photo
+  accepts_nested_attributes_for :gallery_images
+
+  has_attached_file :logo,
+      :storage => :s3,
+      s3_region: 'us-west-1',
+      s3_host_name: 's3-us-west-1.amazonaws.com',
+      styles: { medium: "300x300>", thumb: "100x100>" },
+      :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+
+  validates_with AttachmentSizeValidator, attributes: :logo, less_than: 1.megabytes
+
+  # validates_attachment_file_name :logo, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
+
+  do_not_validate_attachment_file_type :logo
+
+
+
+
+
+  def s3_credentials
+    {:bucket => Rails.application.secrets.s3_bucket_name,
+    :access_key_id => Rails.application.secrets.aws_access_key_id ,
+    :secret_access_key => Rails.application.secrets.aws_secret_access_key}
+  end
+
+  def name local='en'
+    local == 'en' ? name_en : name_cn
+  end
 
 
 end
