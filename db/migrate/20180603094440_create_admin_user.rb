b8f438f560521e3b3128be08381d7c316ec019da
Nadeem Yasin <nadeem@westagilelabs.com>
2018-06-03 14:51:37 +0500

    Adding migration to create admin user

--- db/migrate/20180603094440_create_admin_user.rb
+++ db/migrate/20180603094440_create_admin_user.rb
@@ -1,7 +1,5 @@
 class CreateAdminUser < ActiveRecord::Migration[5.1]
   def change
-    create_table :admin_users do |t|
-      AdminUser.create!(email: 'admin@feelingjy.com', password: '!32AdCDEep$$%#AADfij0909034ASSSCd#@@ASss!!!!casfasdfpjjjnnCCCSSSCDDD', password_confirmation: '!32AdCDEep$$%#AADfij0909034ASSSCd#@@ASss!!!!casfasdfpjjjnnCCCSSSCDDD')
-    end
+    AdminUser.create!(email: 'admin@feelingjy.com', password: '!32AdCDEep$$%#AADfij0909034ASSSCd#@@ASss!!!!casfasdfpjjjnnCCCSSSCDDD', password_confirmation: '!32AdCDEep$$%#AADfij0909034ASSSCd#@@ASss!!!!casfasdfpjjjnnCCCSSSCDDD')
   end
 end
